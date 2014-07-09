4faafdc626d240957f114ac2b7b4feaf019eb8d4
Hegesippe <Hegesippe@lofn.dist>
2014-07-09 18:10:39 +0200

    Modified to have only a point list in input file Travelling now adds up to a certain length instead of amount

--- java/src/vrp/model/Saving.java
+++ java/src/vrp/model/Saving.java
@@ -1,24 +1,25 @@
 package vrp.model;
 
-public class Saving implements Comparable<Saving>{
-	public int val;
-	public Node from;
-	public Node to;
-	
-	public Saving(int v,Node f,Node t){
-		val = v;
-		from = f;
-		to = t;
-	}
+public class Saving implements Comparable<Saving> {
 
-	@Override
-	public int compareTo(Saving o) {
-		if(o.val<this.val){
-			return -1;
-		}else if(o.val == this.val){
-			return 0;
-		}else{
-			return 1;
-		}
-	}
+    public int val;
+    public Node from;
+    public Node to;
+
+    public Saving(int v, Node f, Node t) {
+        val = v;
+        from = f;
+        to = t;
+    }
+
+    @Override
+    public int compareTo(Saving o) {
+        if (o.val < this.val) {
+            return -1;
+        } else if (o.val == this.val) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
--- java/src/vrp/program/Start.java
+++ java/src/vrp/program/Start.java
@@ -4,28 +4,26 @@ import java.io.IOException;
 
 public class Start {
 
-	public static void main(String[] args) throws IOException{
-		
-		if(args.length!=2){
-			System.out.println("Two parameters are obligatory. Filename and  'sweep' or 'clark' to choose the algorithm");
-			System.out.println("USAGE: Start [filename] [sweep|clark]");
-		}
-		
-		String fileName = args[0];
-		
-		VRPProgram.loadData(fileName);
-		Stopwatch s = new Stopwatch();
-		s.start();
-		
-		if(args[1].equals("sweep")){
-			String output = VRPProgram.sweep();
-			System.err.println(output);
-		}else{
-			VRPProgram.clarkWright();
-		}
-		s.stop();
-		System.out.println(s.toString());
-		//MyUtils.generateRandomNodes(100,300,15);
+    public static void main(String[] args) throws IOException {
 
-	}
+//		if(args.length!=2){
+//			System.out.println("Two parameters are obligatory. Filename and  'sweep' or 'clark' to choose the algorithm");
+//			System.out.println("USAGE: Start [filename] [sweep|clark]");
+//		}
+//		String fileName = args[0];
+        VRPProgram.loadData("input.points");
+        Stopwatch s = new Stopwatch();
+        s.start();
+
+//		if(args[1].equals("sweep")){
+        String output = VRPProgram.sweep();
+        System.err.println(output);
+//		}else{
+//			VRPProgram.clarkWright();
+//		}
+        s.stop();
+        System.out.println(s.toString());
+        //MyUtils.generateRandomNodes(100,300,15);
+
+    }
 }
